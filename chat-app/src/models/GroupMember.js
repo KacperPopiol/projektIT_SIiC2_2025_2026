51a55cf7ae9038cadfdeb70dfc65b2a3db2d2a52
KacperPopiol <115403791+KacperPopiol@users.noreply.github.com>
2025-12-08 16:03:55 +0100

    initial changes for group archive

--- chat-app/src/models/GroupMember.js
+++ chat-app/src/models/GroupMember.js
@@ -1,56 +1,64 @@
 module.exports = (sequelize, DataTypes) => {
-	const GroupMember = sequelize.define(
-		'GroupMember',
-		{
-			member_id: {
-				type: DataTypes.INTEGER,
-				primaryKey: true,
-				autoIncrement: true,
-			},
-			group_id: {
-				type: DataTypes.INTEGER,
-				allowNull: false,
-				references: {
-					model: 'groups',
-					key: 'group_id',
-				},
-			},
-			user_id: {
-				type: DataTypes.INTEGER,
-				allowNull: false,
-				references: {
-					model: 'users',
-					key: 'user_id',
-				},
-			},
-			status: {
-				type: DataTypes.ENUM('pending', 'accepted', 'rejected'),
-				defaultValue: 'pending',
-			},
-			role: {
-				type: DataTypes.ENUM('creator', 'member'),
-				defaultValue: 'member',
-			},
-			joined_at: {
-				type: DataTypes.DATE,
-				allowNull: true,
-			},
-			left_at: {
-				type: DataTypes.DATE,
-				allowNull: true,
-			},
-		},
-		{
-			tableName: 'group_members',
-			timestamps: false,
-			indexes: [
-				{
-					unique: true,
-					fields: ['group_id', 'user_id'],
-				},
-			],
-		}
-	)
+  const GroupMember = sequelize.define(
+    "GroupMember",
+    {
+      member_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      group_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: "groups",
+          key: "group_id",
+        },
+      },
+      user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: "users",
+          key: "user_id",
+        },
+      },
+      status: {
+        type: DataTypes.ENUM("pending", "accepted", "rejected"),
+        defaultValue: "pending",
+      },
+      role: {
+        type: DataTypes.ENUM("creator", "member"),
+        defaultValue: "member",
+      },
+      joined_at: {
+        type: DataTypes.DATE,
+        allowNull: true,
+      },
+      left_at: {
+        type: DataTypes.DATE,
+        allowNull: true,
+      },
+      is_archived: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+      },
+      archived_at: {
+        type: DataTypes.DATE,
+        allowNull: true,
+      },
+    },
+    {
+      tableName: "group_members",
+      timestamps: false,
+      indexes: [
+        {
+          unique: true,
+          fields: ["group_id", "user_id"],
+        },
+      ],
+    },
+  );
 
-	return GroupMember
-}
+  return GroupMember;
+};
